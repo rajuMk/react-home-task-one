aca23a3bad71174b4f0ddb426b84677664e5636c
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-14 18:39:41 +0530

    Create ResultContainer.js

---
+++ src/ResultContainer/ResultContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ResultContainer.css'
+
+const ResultContainer = () => {
+    return (
+        <>
+            <div className="ResultContMain">
+               raju
+            </div>
+        </>
+    )
+}
+
+export default ResultContainer;
