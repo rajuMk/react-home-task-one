effd8a57eab52d7cdb6456c2fc67a9c01687d1cc
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-14 18:39:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import Header from './Header/Header'
 import Search from './Search/Search'
 import MoviesFound from './MoviesFound/MoviesFound'
+import FooterContainer from './FooterContainer/FooterContainer'
+import SearchContainer from './SearchContainer/SearchContainer'
+import SortContainer from './SortContainer/SortContainer'
+import ResultContainer from './ResultContainer/ResultContainer'
+import SearchBy from './SearchBy/SearchBy'
+import SortBy from './SortBy/SortBy'
+
 
 function App() {
   return (
     <div className="App">
-          <Header/>
-          <Search/>
-          <MoviesFound/>
+      <SearchContainer>
+        <Header />
+        <Search />
+        <SearchBy />
+      </SearchContainer>
+      <SortContainer>
+        <MoviesFound />
+        <SortBy />
+      </SortContainer>
+      <ResultContainer>
+        
+      </ResultContainer>
+      <FooterContainer />
     </div>
   );
 
