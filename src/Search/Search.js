9a5af32248c91d86122b6d72acff1170a16171cd
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-14 18:39:50 +0530

    Update Search.js

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,16 +3,9 @@ import './Search.css'
 
 const Search = () =>{
     return(
-        <div className="searchMain container">
-            <div>
+        <div className="container">
                 <input type="text" class="form-control"></input>
-                <button class="btn btn-primary mt-1">SEARCH</button>
-            </div>
-            <div class="mt-3">
-                <span class="ml-2">SEARCH BY</span>
-                <button class="ml-2">TITLE</button>
-                <button class="ml-2">GENRE</button>
-            </div>
+                <button class="btn btn-success mt-1">SEARCH</button>
         </div>
     )
 }
