4d2c76d484e58f5f1fab43486aa6daa451ca670e
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-14 18:38:47 +0530

    14102019

---
+++ src/FooterContainer/FooterContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FooterContainer.css'
+
+const FooterContainer = () => {
+    return (
+        <>
+            <label className="FootContainerMain"><b>netflix</b>roulette</label>
+        </>
+    )
+}
+
+export default FooterContainer;
