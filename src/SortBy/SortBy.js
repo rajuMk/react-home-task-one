537bd31cea4936031bf660313b13d32adbe19c09
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-14 18:40:08 +0530

    Update SortBy.js

--- src/SortBy/SortBy.js
+++ src/SortBy/SortBy.js
@@ -1,2 +1,16 @@
-import React from 'react'
-import 'SoryBy.css'
+import React from 'react';
+import './SortBy.css'
+
+const SortBy = () =>{
+    return(
+        <>
+            <div class="mt-3 sortByMain">
+                <span class="ml-2">SEARCH BY</span>
+                <button class="ml-2">TITLE</button>
+                <button class="ml-2">GENRE</button>
+            </div>
+        </>
+    )
+}
+
+export default SortBy;
