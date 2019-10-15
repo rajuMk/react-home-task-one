396fb1008622f6ad3d7d6f649f34a2b0cd069c4b
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-15 19:04:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from './Header/Header'
 import Search from './Search/Search'
@@ -9,27 +9,80 @@ import SortContainer from './SortContainer/SortContainer'
 import ResultContainer from './ResultContainer/ResultContainer'
 import SearchBy from './SearchBy/SearchBy'
 import SortBy from './SortBy/SortBy'
+import MovieTile from './MovieTile/MovieTile';
 
+class App extends Component{
+  state = {
+    movies: [],
+    filteredMovies: []
+  }
+  componentDidMount(){
+    fetch('https://reactjs-cdp.herokuapp.com/movies')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        movies:data.data,
+        filteredMovies:data.data
+      });
+    })
+    .catch(error => {
+      console.log('Looks like there was a problem: \n', error);
+    }); 
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <SearchContainer>
-        <Header />
-        <Search />
-        <SearchBy />
-      </SearchContainer>
-      <SortContainer>
-        <MoviesFound />
-        <SortBy />
-      </SortContainer>
-      <ResultContainer>
-        
-      </ResultContainer>
-      <FooterContainer />
-    </div>
-  );
+  filterList(serchString){
+    let filteredMovies = this.state.movies;
+    if(serchString.length>0){
+    filteredMovies = filteredMovies.filter((movie, index) => {
+      return movie.title.toLowerCase().search(
+        serchString.toLowerCase()) !== -1;
+    });
+  }
+    this.setState({filteredMovies: filteredMovies});
+  
+  }
 
+onChangeHandler = (e) => {
+  console.log(e.target.value);
+  this.filterList(e.target.value)
 }
+  
+  render(){
+    
+    let Movies = (
+      <div>
+        {this.state.filteredMovies.map( ( fm, index ) => {
+          return <MovieTile
+            title={fm.title}
+            runtime={fm.runtime}
+            poster_path = {fm.poster_path}/>
+        } )}
+      </div>
+    );
+
+  
+
+    return (
+      <div className="App">        
+        <SearchContainer>
+          <Header />
+          <Search change={this.onChangeHandler}/>
+          <SearchBy />
+        </SearchContainer>
+        <SortContainer>
+          <MoviesFound />
+          <SortBy />
+        </SortContainer>
+        <ResultContainer>
+          {Movies}
+        </ResultContainer>
+
+        {/* <MovieTile Runtime={this.state.filteredMovies[0].runtime}  Title={this.state.filteredMovies[0].title}/> */}
+        <FooterContainer movies={this.state.movies}/>
+      </div>
+    )
+ }
+}
+
 
 export default App;
