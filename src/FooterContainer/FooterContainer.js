ed79ed74b48c46f667052a21f2dd764c8093bdd2
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-15 19:04:38 +0530

    Update FooterContainer.js

--- src/FooterContainer/FooterContainer.js
+++ src/FooterContainer/FooterContainer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './FooterContainer.css'
 
-const FooterContainer = () => {
+const FooterContainer = (props) => {
+    console.log(props);
     return (
         <>
             <label className="FootContainerMain"><b>netflix</b>roulette</label>
