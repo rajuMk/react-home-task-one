b0d2c80bbc9692c101b720c7487aa64225b6409c
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-15 19:04:49 +0530

    Update Search.js

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Search.css'
 
-const Search = () =>{
+const Search = (props) =>{
     return(
         <div className="container">
-                <input type="text" class="form-control"></input>
+                <input type="text" class="form-control" onChange={(event)=>props.change(event)}></input>
                 <button class="btn btn-success mt-1">SEARCH</button>
         </div>
     )
