a1ac31de41a8aa82d806f253121be7ffba88e88d
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-17 18:29:33 +0530

    Update SortBy.js

--- src/SortBy/SortBy.js
+++ src/SortBy/SortBy.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SortBy.css'
 
-const SortBy = () =>{
+const SortBy = (props) =>{
     return(
         <>
             <div class="mt-3 sortByMain">
-                <span class="ml-2">SEARCH BY</span>
-                <button class="ml-2">TITLE</button>
-                <button class="ml-2">GENRE</button>
+                <span class="ml-2">SORT BY</span>
+                <button class="ml-2" onClick={props.click} value="releaseDate">Release date</button>
+                <button class="ml-2" onClick={props.click} value="rating">Rating</button>
             </div>
         </>
     )
