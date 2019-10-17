80bf322447f0e120ebc3113eba7a7dc34d7a5d99
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-17 18:29:29 +0530

    Update SearchBy.js

--- src/SearchBy/SearchBy.js
+++ src/SearchBy/SearchBy.js
@@ -6,8 +6,8 @@ const SearchBy = (props) => {
         <>
             <div className="container searchByMain mt-3">
                 <span class="ml-2">SEARCH BY</span>
-                <button class="ml-2 btn btn-primary">TITLE</button>
-                <button class="ml-2 btn btn-info">GENRE</button>
+                <button class="ml-2 btn btn-primary" onClick={props.click} value="title">TITLE</button>
+                <button class="ml-2 btn btn-info" onClick={props.click} value="genre">GENRE</button>
             </div>
         </>
     )
