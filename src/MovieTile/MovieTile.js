19f9260a07b89bc89d69dc7f328894087860a124
RajuK <rajuvarmak@gmail.com>
2019-10-18 09:26:28 +0530

    Update MovieTile.js

--- src/MovieTile/MovieTile.js
+++ src/MovieTile/MovieTile.js
@@ -11,6 +11,8 @@ const MovieTile = (props) => {
                 <label>movie title is: <span>{props.title} </span> </label>
                 <label>movie run time is <span>{props.runtime} </span> Min </label>
                 <label>Movie Genre: <span>{props.genre}</span></label>
+                <label>Movie release date: <span>{props.release_date}</span></label>
+                <label>Movie vote average: <span>{props.vote_average}</span></label>
             </div>
         </>
     )
