5cb8c0cee8a45994ba13b50ed6c4ff843f7ada70
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-21 16:21:38 +0530

    Create MovieFDContainer.js

---
+++ src/Containers/MovieFDContainer/MovieFDContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './MovieFDContainer.css'
+
+const MovieFDContainer = () =>{
+
+    return(
+        <>
+            <div class="mt-3 sortByMain">
+                <h1>Movie Full Details Container</h1>
+            </div>
+        </>
+    )
+}
+
+// class App extends Component{
+//     state = {
+//         movies: [],
+//         filteredMovies: [],
+//         searchBy: "title",
+//         sortBy: "releaseDate"
+//       }
+// }
+
+export default MovieFDContainer;
