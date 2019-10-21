a7e9e83d37a4648999b7562a14534c008b80b001
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-21 20:29:08 +0530

    Update MovieTile.js

--- src/MovieTile/MovieTile.js
+++ src/MovieTile/MovieTile.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import './MovieTile.css'
-
+import './MovieTile.css';
+import { Link } from "react-router-dom";
 
 const MovieTile = (props) => {
     
+    let url = `/EndPage/${props.id}`;
+    
     return (
-       
             <>
-                <div className="MovieTileMain">
-                    <img src={props.poster_path} alt="No Movie found"></img>
-                    <label>movie title is: <span>{props.title} </span> </label>
-                    <label>movie run time is <span>{props.runtime} </span> Min </label>
-                    <label>Movie Genre: <span>{props.genre}</span></label>
-                    <label>Movie release date: <span>{props.release_date}</span></label>
-                    <label>Movie vote average: <span>{props.vote_average}</span></label>
-                </div>
+                <Link to={url}>
+                    <div className="MovieTileMain">
+                        <img src={props.poster_path} alt="No Movie found"></img>
+                        <label>movie title is: <span>{props.title} </span> </label>
+                        <label>movie run time is <span>{props.runtime} </span> Min </label>
+                        <label>Movie Genre: <span>{props.genre}</span></label>
+                        <label>Movie release date: <span>{props.release_date}</span></label>
+                        <label>Movie vote average: <span>{props.vote_average}</span></label>
+                    </div>
+                </Link>
+                
             </>
       
     )
