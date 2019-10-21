eb2b11d247a0047fab7c6a7dbfff7bb3e97afd28
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-21 16:21:43 +0530

    Update MovieTile.js

--- src/MovieTile/MovieTile.js
+++ src/MovieTile/MovieTile.js
@@ -5,16 +5,18 @@ import './MovieTile.css'
 const MovieTile = (props) => {
     
     return (
-        <>
-            <div className="MovieTileMain">
-                <img src={props.poster_path} alt="No Movie found"></img>
-                <label>movie title is: <span>{props.title} </span> </label>
-                <label>movie run time is <span>{props.runtime} </span> Min </label>
-                <label>Movie Genre: <span>{props.genre}</span></label>
-                <label>Movie release date: <span>{props.release_date}</span></label>
-                <label>Movie vote average: <span>{props.vote_average}</span></label>
-            </div>
-        </>
+       
+            <>
+                <div className="MovieTileMain">
+                    <img src={props.poster_path} alt="No Movie found"></img>
+                    <label>movie title is: <span>{props.title} </span> </label>
+                    <label>movie run time is <span>{props.runtime} </span> Min </label>
+                    <label>Movie Genre: <span>{props.genre}</span></label>
+                    <label>Movie release date: <span>{props.release_date}</span></label>
+                    <label>Movie vote average: <span>{props.vote_average}</span></label>
+                </div>
+            </>
+      
     )
 }
 
