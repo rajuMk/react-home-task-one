012db8742caec7d267c11d0f73d4f2c5b850e6c1
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-22 16:31:34 +0530

    Update RelatedGenre.js

--- src/Components/RelatedGenre/RelatedGenre.js
+++ src/Components/RelatedGenre/RelatedGenre.js
@@ -9,11 +9,11 @@ const RelatedGenre = (props) => {
             <>
                 <div className="RelatedGenre">
                     <img src={props.poster_path} alt="No Movie found"></img>
-                    <label>movie title is: <span>{props.title} </span> </label>
-                    <label>movie run time is <span>{props.runtime} </span> Min </label>
-                    <label>Movie Genre: <span>{props.genre}</span></label>
-                    <label>Movie release date: <span>{props.release_date}</span></label>
-                    <label>Movie vote average: <span>{props.vote_average}</span></label>
+                    <label>movie title is: {props.title} </label> 
+                   <label> movie run time is {props.runtime} </label>
+                     <label>Movie Genre: {props.genre}</label>
+                     <label>Movie release date:{props.release_date}</label>
+                     <label>Movie vote average:{props.vote_average}</label>
                 </div>
             
             </>
