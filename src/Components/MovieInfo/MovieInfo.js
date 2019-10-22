cf4bce6e4252a031d9abadcb6f866d6393a0e0b1
Matharaju Karri <Matharaju_Karri@epam.com>
2019-10-22 16:13:56 +0530

    Update MovieInfo.js

--- src/Components/MovieInfo/MovieInfo.js
+++ src/Components/MovieInfo/MovieInfo.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import './MovieInfo.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const MovieInfo = (props) => {
-    
+    let genres = props.genres.join(",")
     return (
-            <>
-                <div>
-                    <Link to='/Home'>
-                        <button style={{float:'right'}}>Back to all Movies search</button>
-                    </Link>                
-                </div>
-                <div className="MovieInfoMain">
-                    {/* <div>Movie Info File</div> */}
-                    <img src={props.poster_path} alt="No Movie found"></img>
-                    <label>movie title is: <span>{props.title} </span> </label>
-                    <label>movie run time is <span>{props.runtime} </span> Min </label>
-                    <label>Movie vote average: <span>{props.vote_average}</span></label>
-                    <label>Tag Line: <span>{props.tagline}</span></label>
-                    <label>Movie release date: <span>{props.release_date}</span></label>
-                    <label>Over View: <span>{props.release_date}</span></label>
-                    <label>Movie Genre: <span>{props.genre}</span></label>
-                </div>
-                
-            </>
-      
+        <>
+            <div className="MovieInfoMain">
+                {/* <div>Movie Info File</div> */}
+                <img src={props.poster_path} alt="No Movie found"></img>
+                <span> movie title is: <b>{props.title}</b> </span>
+                <span> movie genre: <b>{genres}</b> </span>
+                <span>Movie vote average:<b>{props.vote_average}</b> </span>
+                <span>Tag Line:<b>{props.tagline}</b> </span>
+                <span>Movie release date:<b>{props.release_date}</b> </span>
+                {/* <span>Over View:<b>{props.release_date}</b> </span> */}
+                <Link to='/Home'>
+                    <button class="btn btn-success" style={{ float: 'right' }}>Back to all Movies search</button>
+                </Link>
+            </div>
+        </>
+
     )
 }
 
